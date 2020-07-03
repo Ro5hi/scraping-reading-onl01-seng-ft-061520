c30aba39dd0ebc0a83b053997ca8122b8569e2ac
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-03 16:53:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,7 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 Nokogiri::HTML(html)
+
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))doc.css(".headline.260IBN")
+
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
